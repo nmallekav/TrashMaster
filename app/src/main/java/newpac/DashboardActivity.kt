8429959c7506e0a8bf88ba8ce103e96a715e98e1
Nishanth Mallekav <nmallekav@outlook.com>
2020-12-06 13:42:25 -0500

    merged

--- app/src/main/java/newpac/DashboardActivity.kt
+++ app/src/main/java/newpac/DashboardActivity.kt
@@ -1,5 +1,6 @@
 package newpac
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -18,7 +19,46 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        mSchedulePickup = findViewById(R.id.schedule_pickup)
+        mSchedulePickup!!.setOnClickListener {schedulePickupMethod()}
+
+        mReportSlolen = findViewById(R.id.report_stolen)
+        mReportSlolen!!.setOnClickListener {reportStolenMethod()}
+
+        mMissedTrash = findViewById(R.id.missed_trash)
+        mMissedTrash!!.setOnClickListener {missedTrashMethod()}
+
+        mReportTrash = findViewById(R.id.report_trash)
+        mReportTrash!!.setOnClickListener {reportTrashMethod()}
+
         mGpsTracker = findViewById(R.id.gps_tracker)
+        mGpsTracker!!.setOnClickListener {gpsTrackerMethod()}
+
+
     }
 
+    private fun schedulePickupMethod() {
+        val intent = Intent(this@DashboardActivity, SchedulePickupActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun reportStolenMethod() {
+        val intent = Intent(this@DashboardActivity, ReportStolenActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun missedTrashMethod() {
+        val intent = Intent(this@DashboardActivity, MissedTrashActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun reportTrashMethod() {
+        val intent = Intent(this@DashboardActivity, ReportTrashActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun gpsTrackerMethod() {
+        val intent = Intent(this@DashboardActivity, GarbageTruckGPSMarkOnMap::class.java)
+        startActivity(intent)
+    }
 }
